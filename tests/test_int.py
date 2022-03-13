15b8c86bcf6b60d07311cefa65bacbc9d9e82aa7
Ian Collinge <iancollinge@gmail.com>
2022-03-13 14:31:38 +0000

    Delete test_int.py

--- tests/test_int.py
+++
@@ -1,9 +0,0 @@
-def test_empty_validation(self):
-        self.submit_input('')
-        self.assertIn(url_for('index'), self.driver.current_url)
-
-        text = self.driver.find_element_by_xpath('<XPath>').text
-        self.assertIn("The name field can't be empty!", text)
-
-        entries = TestBase.query.all()
-        self.assertEqual(len(entries), 0) # database should be empty
